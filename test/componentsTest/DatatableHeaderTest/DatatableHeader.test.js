d49dc70eeba3107412ef2caf13dcf92c154ddb6a
MorganDbs <mailmorgandubois@gmail.com>
2019-06-12 14:11:24 +0200

    new tests DatatableHeader

---
+++ test/componentsTest/DatatableHeaderTest/DatatableHeader.test.js
@@ -0,0 +1,36 @@
+import React from "react";
+import configureStore from "redux-mock-store";
+import { Provider } from "react-redux";
+import { shallow, mount } from "enzyme";
+import DatatableHeader from "../../../src/components/DatatableHeader/DatatableHeader";
+import { storeSample } from "../../../data/samples";
+
+const mockStore = configureStore();
+const store = mockStore(storeSample);
+
+describe("DatatableHeader component", () => {
+  it("connected should render without errors", () => {
+    const wrapper = shallow(
+      <Provider store={store}>
+        <DatatableHeader />
+      </Provider>
+    );
+    expect(wrapper.find("Connect(DatatableHeader)")).toHaveLength(1);
+  });
+
+  describe("should render", () => {
+    const wrapper = mount(
+      <Provider store={store}>
+        <DatatableHeader />
+      </Provider>
+    );
+
+    it("a title", () => {
+      expect(wrapper.find("div.title")).toHaveLength(1);
+    });
+
+    it("an export widget", () => {
+      expect(wrapper.find("ExportRows")).toHaveLength(1);
+    });
+  });
+});
